3160ea187569a5d9c064699959fbc08a6c29dce2
Samuel W. Flint <swflint@flintfam.org>
2023-07-26 21:01:05 -0500

    Add a stub third-time implementation

---
+++ third-time.el
@@ -0,0 +1,37 @@
+;;; third-time.el --- Third Time support for Emacs -*- lexical-binding: t -*-
+
+;; Copyright (C) 2023 Samuel W. Flint <swflint@flintfam.org>
+
+;; Author: Samuel W. Flint <swflint@flintfam.org>
+;; SPDX-License-Identifier: GPL-3.0-or-later
+;; URL: https://git.sr.ht/~swflint/busylight
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "25.1"))
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+;;
+;; TODO
+
+;;; Code:
+
+
+(provide 'third-time)
+
+;;; third-time.el ends here
